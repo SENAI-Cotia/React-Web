6af073dd31e29966ad61c4784bc7912dd46943a2
Matheus Zacarias <matheuus412@gmail.com>
2025-09-29 18:58:04 -0300

    feat: middleware protected route

--- example/auth/src/routes.tsx
+++ example/auth/src/routes.tsx
@@ -2,13 +2,21 @@ import { Routes, Route } from "react-router-dom";
 import { Login } from "./pages/Login/Login";
 import { SignIn } from "./pages/SignIn/SignIn";
 import { Home } from "./pages/Home/Home";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/home" element={<Home />} />
+      <Route
+        path="/home"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
