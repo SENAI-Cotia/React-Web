7c309861ade4103ac8671b30206b10b8e8b8857e
Matheus Zacarias <matheuus412@gmail.com>
2025-09-29 18:53:09 -0300

    feat: alterando a mensagem de erro

--- example/auth/src/types/auth.ts
+++ example/auth/src/types/auth.ts
@@ -9,9 +9,13 @@ export type LoginData = z.infer<typeof LoginSchema>;
 
 export const RegisterSchema = LoginSchema.extend({
   name: z.string().min(2),
-  email: z.email(),
-  password: z.string().min(6),
-  confirmPassword: z.string().min(6),
+  email: z.email({ error: "Email inválido" }),
+  password: z
+    .string()
+    .min(6, { message: "Senha deve ter no mínimo 6 caracteres" }),
+  confirmPassword: z
+    .string()
+    .min(6, { message: "Senha deve ter no mínimo 6 caracteres" }),
 }).refine((data) => data.password === data.confirmPassword, {
   message: "Senhas não coincidem",
 });
